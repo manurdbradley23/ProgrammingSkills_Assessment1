94adb20943bad17eeb041bc3d516066fb0c6848d
manurdbradley23 <marcos.brad2023@gmail.com>
2024-11-08 11:34:52 +0400

    Add files via upload

---
+++ 03-Biography.py
@@ -0,0 +1,6 @@
+#Writes the biography of user
+dictionary = {"name": input('whats your name little child...'),
+              "hometown": input('where do you live little child...'),
+              "age": input('whats your age little girl...')}
+#Compiles inputs of the user's bio then outputs it back.
+print(dictionary)
