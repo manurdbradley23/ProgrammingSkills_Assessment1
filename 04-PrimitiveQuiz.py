9114935c98594ad9eb779d0551145cc4fcf195d5
manurdbradley23 <marcos.brad2023@gmail.com>
2024-11-10 17:55:48 +0400

    Add files via upload

---
+++ 04-PrimitiveQuiz.py
@@ -0,0 +1,18 @@
+dict1 ={"What is the capital of Austria:":'Vienna',
+        "What is the capital of Germany:":'Berlin',
+        "What is the capital of Italy:": 'Rome',
+        "What is the capital of Norway:": 'Oslo',
+        "What is the capital of Belgium:": 'Brasilia',
+        "What is the capital of U.K:": 'London',
+        "What is the capital of Croatia:": 'Zagreb',
+        "What is the capital of Denmark:": 'Copenhagen',
+        "What is the capital of Finland:": 'Helsinki',
+        "What is the capital of Czechia:": 'Prague'}
+def questions_answers(question,answer):
+        answers = input(question)
+        if answers.lstrip().lower() == answer.lstrip().lower():
+                print("Wow you got that correct!")
+        else:
+         print("The supreme leader has scheduled your execution in 10 minutes.")
+for k,s in dict1.items():
+        questions_answers(k,s)
